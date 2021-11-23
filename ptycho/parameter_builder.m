2c527806cf2428e4badceca58f2b0b38424286a7
Chenyu Zhang <czhang376@wisc.edu>
2021-11-23 07:46:43 -0600

    Added files for submitting ptychography jobs on ALTAS cluster

---
+++ ptycho/parameter_builder.m
@@ -0,0 +1,24 @@
+function par = parameter_builder(parfile)
+
+    par = struct;
+    fileID = fopen(parfile, 'r');
+    tline = fgetl(fileID);
+    while ischar(tline)
+        tline = fgetl(fileID);
+        if tline == -1
+            break;
+        end
+    %     disp(tline);
+        temp = split(tline);
+        if size(temp, 1) == 1
+            continue
+        end
+        if ~isnan(str2double(temp{2,1}))
+            par.(temp{1,1}) = str2double(temp{2,1});
+        else
+            par.(temp{1,1}) = temp{2,1};
+        end
+    end
+%     fclose(fileID);
+%     save( parfile, "par");
+end
